c3da812bf07a80402eeb384841fd73d7c6363bf7
Taylor Hanayik <hanayik@gmail.com>
2024-09-11 17:11:21 +0100

    remove useless files

--- packages/docs/src/components/HomepageFeatures/index.js
+++ packages/docs/src/components/HomepageFeatures/index.js
@@ -9,8 +9,7 @@ const FeatureList = [
     description: (
       <>
         NiiVue is a JavaScript library that can be used in any modern web
-        environment. It works in all modern browsers, and can be easily
-        embedded in any web framework including iOS/Android apps and desktop apps using Electron.
+        environment.
       </>
     ),
   },
@@ -18,20 +17,16 @@ const FeatureList = [
     title: 'Simple API',
     description: (
       <>
-        NiiVue is designed to be easy to use. Just include the library in your 
-        project and start rendering over <strong>30 medical image formats</strong>. 
-        NiiVue is platform and framework agnostic.
+        NiiVue is designed to be easy to use and is platform and framework agnostic.
       </>
     ),
   },
   {
-    title: 'Used by the community',
+    title: 'Community',
     // Svg: require('@site/static/img/undraw_docusaurus_react.svg').default,
     description: (
       <>
-        The NiiVue library is used by the neuroimaging community by
-        both large and small projects. It is used by OpenNeuro, FSL, AFNI, Freesurfer, and BrainLife. 
-        Community members have made VS Code extensions, and Jupyter notebook plugins for NiiVue. 
+        The community has made extensions, products, and plug-ins using NiiVue. 
       </>
     ),
   },
